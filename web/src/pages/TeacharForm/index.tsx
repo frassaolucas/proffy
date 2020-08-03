512ad2637f647d1dd6f04c99b589946c49d87f87
Lucas Frassao <frassao.lucas@gmail.com>
2020-08-03 20:36:17 -0300

    created page-header component and styled form for study page

--- web/src/pages/TeacharForm/index.tsx
+++ web/src/pages/TeacharForm/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import PageHeader from '../../components/PageHeader';
 
 const TeacharForm: React.FC = () => {
-  return <h1>Teacher Form</h1>;
+  return (
+    < div id="page-teacher-form" className="contaienr" >
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div >
+  );
 }
 
 export default TeacharForm;
